ea725df7bd1e4e68315f3d819ffa26131bbdf448
Anne Agbo <agboanneananwune@gmail.com>
2022-12-12 17:00:27 +0100

    added an annotation to the userutil class

--- src/main/java/com/example/decapay/utils/UserUtil.java
+++ src/main/java/com/example/decapay/utils/UserUtil.java
@@ -2,7 +2,9 @@ package com.example.decapay.utils;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserUtil {
     public String getAuthenticatedUserEmail() {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
