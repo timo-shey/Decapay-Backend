eb0a16d5c14ef9be8531899f09863940bb87fb90
seunrae <oluseun.ademilua@gmail.com>
2022-12-14 22:41:45 +0100

    updated user login implementation fix

--- src/main/java/com/example/decapay/services/UserService.java
+++ src/main/java/com/example/decapay/services/UserService.java
@@ -1,9 +1,13 @@
 package com.example.decapay.services;
 
+import com.example.decapay.pojos.requestDtos.LoginRequestDto;
+import org.springframework.http.ResponseEntity;
 import com.example.decapay.pojos.requestDtos.UserUpdateRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface UserService {
+    public ResponseEntity<String> userLogin(LoginRequestDto loginRequestDto);
+
     ResponseEntity<String> editUser(UserUpdateRequest userUpdateRequest);
 
 }
