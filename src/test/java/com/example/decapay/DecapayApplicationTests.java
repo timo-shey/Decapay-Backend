f12ce98f9ad1545f560e03ce20caa078400b54d5
InnocentOmenka <innocent.omenka@decagon.dev>
2022-12-16 11:21:29 +0100

    test

--- src/test/java/com/example/decapay/DecapayApplicationTests.java
+++ src/test/java/com/example/decapay/DecapayApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DecapayApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
