07d565d39c5e655329794a8d9d5b3fa7c07f6c5d
OLUSEUN <oluseun.ademilua@gmail.com>
2022-12-19 17:28:56 +0100

    created expense classes and methods

--- src/main/java/com/example/decapay/services/ExpenseService.java
+++ src/main/java/com/example/decapay/services/ExpenseService.java
@@ -1,2 +1,9 @@
-package com.example.decapay.services;public class ExpenseService {
+package com.example.decapay.services;
+
+import com.example.decapay.pojos.expenseDto.ExpenseRequestDto;
+import com.example.decapay.pojos.expenseDto.ExpenseResponseDto;
+import org.springframework.http.ResponseEntity;
+
+public interface ExpenseService {
+    ResponseEntity<ExpenseResponseDto> createExpense(ExpenseRequestDto expenseRequestDto, Long lineId);
 }
--- src/main/java/com/example/decapay/services/impl/ExpenseServiceImpl.java
+++ src/main/java/com/example/decapay/services/impl/ExpenseServiceImpl.java
@@ -1,2 +1,34 @@
-package com.example.decapay.services.impl;public class ExpenseServiceImpl {
+package com.example.decapay.services.impl;
+
+import com.example.decapay.models.Expense;
+import com.example.decapay.pojos.expenseDto.ExpenseRequestDto;
+import com.example.decapay.pojos.expenseDto.ExpenseResponseDto;
+import com.example.decapay.repositories.ExpenseRepository;
+import com.example.decapay.services.ExpenseService;
+import com.example.decapay.utils.UserUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseServiceImpl implements ExpenseService {
+    private final ExpenseRepository expenseRepository;
+    private final UserUtil userUtil;
+
+
+    @Override
+    public ResponseEntity<ExpenseResponseDto> createExpense(ExpenseRequestDto expenseRequestDto, Long lineId) {
+
+        Expense expense = new Expense();
+        expense.setAmount(expenseRequestDto.getAmount());
+
+        expenseRepository.save(expense);
+
+        ExpenseResponseDto expenseResponseDto = new ExpenseResponseDto();
+        expenseResponseDto.setAmount(expense.getAmount());
+        expenseResponseDto.setCreatedAt(expense.getCreatedAt());
+
+        return ResponseEntity.ok(expenseResponseDto);
+    }
 }
