fef81a3aec118773774210122369e5d48469895b
Anne Agbo <agboanneananwune@gmail.com>
2022-12-21 09:37:14 +0100

    budget categeory

---
+++ src/main/java/com/example/decapay/repositories/BudgetCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.decapay.repositories;
+
+import com.example.decapay.models.BudgetCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BudgetCategoryRepository extends JpaRepository<BudgetCategory, Long> {
+
+
+}
