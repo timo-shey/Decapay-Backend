e6c39fbf43971a244bddd6a48960d6370c68e0de
nurudeen <nurudain30@gmail.comn>
2022-12-21 21:31:12 +0100

    test updated

--- src/test/java/com/example/decapay/services/impl/BudgetCategoryServiceImpTest.java
+++ src/test/java/com/example/decapay/services/impl/BudgetCategoryServiceImpTest.java
@@ -5,100 +5,111 @@ import com.example.decapay.configurations.security.JwtAuthFilter;
 import com.example.decapay.models.BudgetCategory;
 import com.example.decapay.models.User;
 import com.example.decapay.pojos.requestDtos.BudgetCategoryRequest;
-import com.example.decapay.pojos.responseDtos.BudgetCategoryResponse;
 import com.example.decapay.repositories.BudgetCategoryRepository;
 import com.example.decapay.repositories.TokenRepository;
 import com.example.decapay.repositories.UserRepository;
 import com.example.decapay.services.BudgetCategoryService;
+import com.example.decapay.services.UserService;
 import com.example.decapay.utils.UserIdUtil;
 import com.example.decapay.utils.UserUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 import java.util.Optional;
-import static org.mockito.ArgumentMatchers.any;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BudgetCategoryServiceImpTest {
 
-    @InjectMocks
-    private BudgetCategoryServiceImp budgetCategoryService;
-
-    @Mock
-    private UserRepository userRepository;
+    @Autowired
+    private BudgetCategoryService budgetCategoryService;
 
     @Mock
     private BudgetCategoryRepository budgetCategoryRepository;
 
     @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserIdUtil userIdUtil;
+    @Mock
     private UserUtil userUtil;
 
-   @BeforeEach
-   void setUp(){}
+    @Mock
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Mock
+    private CustomUserDetailService customUserDetailService;
+
+    @Mock
+    private JwtAuthFilter jwtAuthFilter;
+
+    @Mock
+    private TokenRepository tokenRepository;
+
+    private BudgetCategoryRequest budgetCategoryRequest;
+
+    @BeforeEach
+    void setUp() {
+         budgetCategoryService=new BudgetCategoryServiceImp(
+                 budgetCategoryRepository,userRepository, userUtil, userService
+         );
+        budgetCategoryRequest=new BudgetCategoryRequest();
+        budgetCategoryRequest.setName("Food Stuff");
+
+    }
+
     @Test
     void createBudgetCategory() {
 
-        BudgetCategoryRequest budgetCategoryRequest= new BudgetCategoryRequest();
-        budgetCategoryRequest.setName("Provision");
-
         User user=new User();
 
-        LocalDateTime localDateTime= LocalDateTime.now();
         BudgetCategory budgetCategory= new BudgetCategory();
-        budgetCategory.setId(1L);
-        budgetCategory.setUser(user);
-        budgetCategory.setName("Provision");
-        budgetCategory.setCreatedAt(localDateTime);
-        budgetCategory.setUpdatedAt(localDateTime);
-        budgetCategory.setDeleted(false);
-
-        BudgetCategoryResponse budgetCategoryResponse= new BudgetCategoryResponse();
-        budgetCategoryResponse.setId(budgetCategory.getId());
-        budgetCategoryResponse.setName(budgetCategory.getName());
-
 
         String email="mic@gmail.com";
 
-        when(userUtil.getAuthenticatedUserEmail()).thenReturn(email);
-        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        given(userUtil.getAuthenticatedUserEmail()).willReturn(email);
 
-        when(budgetCategoryRepository.save(any(BudgetCategory.class))).thenReturn(budgetCategory);
+        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
 
         budgetCategoryService.createBudgetCategory(budgetCategoryRequest);
 
-        Assert.assertEquals(budgetCategory.getId(),budgetCategoryResponse.getId());
-        Assert.assertEquals(budgetCategory.getName(),budgetCategoryResponse.getName());
-
-
-
     }
+
     @Test
-    void listBudgetCategory() {
-
-        List<BudgetCategoryResponse> budgetCategoryResponses = new ArrayList<>();
-
-        List<BudgetCategory> budgetCategories= new ArrayList<>();
-
+    final void testDeleteBudgetCategory(){
         User user = new User();
-        given(userUtil.getAuthenticatedUserEmail()).willReturn("peterhamza6@gmail.com");
+       BudgetCategory budgetCategory = new BudgetCategory();
 
-        given(userRepository.findByEmail("peterhamza6@gmail.com")).willReturn(Optional.of(user));
+        //stub user object
+        user.setId(1L);;
+        user.setEmail("testing@gmail.com");
 
-        lenient().when(budgetCategoryRepository.findByUser(user)).thenReturn(budgetCategories);
+        //stub budgetCategory object
+        budgetCategory.setId(1L);
+        budgetCategory.setUser(user);
 
-        lenient().when(budgetCategoryService.listBudgetCategory()).thenReturn(budgetCategoryResponses);
+        when(userService.getUserByEmail(anyString())).thenReturn(user);
+        when(userUtil.getAuthenticatedUserEmail()).thenReturn("testing@gmail.com");
+        when(budgetCategoryRepository.findById(1L)).thenReturn(Optional.of(budgetCategory));
 
+        budgetCategoryService.deleteBudgetCategory(budgetCategory.getId());
+        verify(budgetCategoryRepository).save(budgetCategory);
+        verify(budgetCategoryRepository,times(1)).save(any(BudgetCategory.class));
     }
-
 }
